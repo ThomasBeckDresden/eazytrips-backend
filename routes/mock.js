7c5df8fd33fe7c90900560068a1ce1c326f3f7ab
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-07-19 17:31:53 +0200

    var to const - adjust routes to /mock --> returns mockdata.json

--- routes/mock.js
+++ routes/mock.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mockData = require("../public/mock_data.json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.json(mockData);
 });
 
 module.exports = router;
