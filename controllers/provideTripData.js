aeb9aa2e344e59518fd918363caf0498112b3381
Franz <franz.hanschmidt@posteo.de>
2021-07-29 21:29:49 +0200

    fix res json

--- controllers/provideTripData.js
+++ controllers/provideTripData.js
@@ -38,14 +38,17 @@ const provideTripDataFinal = (req, res, next) => {
   //   timeSlots,
   // };
 
-  const tripDataNoAccomm = tripData.trip.map((day) => {
-    const locationNoAccomm = day.locations.filter(
-      (location) => location.place_id !== "accommodation"
-    );
-    return { ...day, locations: locationNoAccomm };
-  });
+  // KEEP IN FINAL VERSION
+  // const tripDataNoAccomm = tripData.trip.map((day) => {
+  //   const locationNoAccomm = day.locations.filter(
+  //     (location) => location.place_id !== "accommodation"
+  //   );
+  //   return { ...day, locations: locationNoAccomm };
+  // });
 
-  res.json({ ...tripData, trip: tripDataNoAccomm });
+  // res.json({ ...tripData, trip: tripDataNoAccomm });
+
+  res.json(tripData);
 };
 
 module.exports = { provideTripDataFinal, provideTripDataRaw };
