bf1812a333fccd01d8feef0cc91dfdafde9f65ad
Franz <franz.hanschmidt@posteo.de>
2021-07-29 19:49:26 +0200

    fix delete accommo

--- controllers/provideTripData.js
+++ controllers/provideTripData.js
@@ -38,10 +38,11 @@ const provideTripDataFinal = (req, res, next) => {
   //   timeSlots,
   // };
 
-  const tripDataNoAccomm = tripData.trip.forEach((day) => {
+  const tripDataNoAccomm = tripData.trip.map((day) => {
     const locationNoAccomm = day.locations.filter(
       (location) => location.place_id !== "accommodation"
     );
+    return { ...day, locations: locationNoAccomm };
   });
 
   res.json(tripDataNoAccomm);
