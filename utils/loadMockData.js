b8ea514e090a8f3a1944acec3d3f12d7bb580d61
Franz <franz.hanschmidt@posteo.de>
2021-07-29 16:14:46 +0200

    fix mock origin

--- utils/loadMockData.js
+++ utils/loadMockData.js
@@ -1,5 +1,5 @@
-const mockDataRaw = require("./mockData/mockDataRawBarca.json");
-const mockDataTrip = require("./mockData/mockDataTripBarca.json");
+const mockDataRaw = require("../mockData/mockDataRawBarca.json");
+const mockDataTrip = require("../mockData/mockDataTripBarca.json");
 
 const loadMockData = (req, res, next) => {
   req.mockData = { tripDataRaw: mockDataRaw, tripData: mockDataTrip };
