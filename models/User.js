b9f1d3ee90c07718f6f7f95f4a2e60931e83a4c2
Jugesh <jugeshkumar0208@gmail.com>
2021-08-02 14:52:00 +0200

    just pushing the data again

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const jwt = require('jsonwebtoken');
 
 const userSchema = new Schema({
     first_name: { type: String, min: 2, max: 50, required: true },
@@ -7,6 +8,13 @@ const userSchema = new Schema({
     email: { type: String, min: 2, max: 50, required: true, unique: true },
     password: { type: String, required: true },
 });
+
+userSchema.methods.createToken = function () {
+    const payload = { id: this._id, email: this.email };
+    const secretKey = process.env.JWT_SECRET;
+    const token = jwt.sign(payload, secretKey);
+    return token;
+}
 
 const User = mongoose.model("User", userSchema);
 
