c807132387dd0925dfc4f7b0c631f57c60f11982
Franz <franz.hanschmidt@posteo.de>
2021-08-03 14:47:43 +0200

    fix type adadption in autocomplete

--- middleware/getPlacesSuggestions.js
+++ middleware/getPlacesSuggestions.js
@@ -1,17 +1,25 @@
 const { default: axios } = require("axios");
-const mockCitySuggestions = require("../mockData/mockCitySuggestions.json");
+const Diacritics = require("diacritic");
 
 const getPlacesSuggestions = async (req, res, next) => {
-  const { sessionToken, prefix } = req.body;
+  const { sessionToken, prefix, type } = req.body;
   const apiGoogle = process.env.API_KEY_GOOGLE;
 
   //   create session token in FRONTEND
   // make google places request, send data back
+  const regex = /\W+/g;
+  const prefixCleaned = Diacritics.clean(prefix || "")
+    .toLowerCase()
+    .trim()
+    .replace(regex, "%20");
 
   try {
     if (prefix) {
+      console.log("type is", type);
       const { data } =
-        await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${prefix}&types=(cities)&key=${apiGoogle}&sessiontoken=${sessionToken}
+        await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${prefixCleaned}&types=${
+          type === "cities" ? "(cities)" : type
+        }&key=${apiGoogle}&sessiontoken=${sessionToken}
       `);
       console.log("fetched autocomplete");
       console.log(data);
