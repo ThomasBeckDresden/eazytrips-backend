59285ab2b0979838ae6bab90d078e43c9ab954f3
Franz <franz.hanschmidt@posteo.de>
2021-08-03 10:34:08 +0200

    activate google places autocomplete

--- middleware/getPlacesSuggestions.js
+++ middleware/getPlacesSuggestions.js
@@ -8,22 +8,22 @@ const getPlacesSuggestions = async (req, res, next) => {
   //   create session token in FRONTEND
   // make google places request, send data back
 
-  //   try {
-  //     if (prefix) {
-  //       const { data } =
-  //         await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${prefix}&types=(cities)&key=${apiGoogle}&sessiontoken=${sessionToken}
-  //     `);
-  //       console.log("fetched autocomplete");
-  //       console.log(data);
-  //       req.placesSuggestions = data;
-  //     }
-  //     next();
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
+  try {
+    if (prefix) {
+      const { data } =
+        await axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${prefix}&types=(cities)&key=${apiGoogle}&sessiontoken=${sessionToken}
+      `);
+      console.log("fetched autocomplete");
+      console.log(data);
+      req.placesSuggestions = data;
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+  }
 
-  req.placesSuggestions = mockCitySuggestions;
-  next();
+  // req.placesSuggestions = mockCitySuggestions;
+  // next();
 
   // on final selection in front-end: trigger google places details call with session token and id
 };
